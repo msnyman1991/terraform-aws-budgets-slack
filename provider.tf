694fa246a9fdca8e6c29c3f0db58d80bf8f1a8d7
Morné Snyman <82025795+msnyman1991@users.noreply.github.com>
2023-08-14 16:49:53 +0200

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,5 @@
+provider "awscc" {
+  # Configure the required arguments for the AWS Cloud Control provider
+  region = "eu-west-1" # Replace with your desired AWS region
+  # Add any other necessary configuration options
+}
