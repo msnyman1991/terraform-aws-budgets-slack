c0f02d62f7dbc466033fdbda3166fc5a628d6e7a
Morné Snyman <82025795+msnyman1991@users.noreply.github.com>
2023-08-14 16:49:28 +0200

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "email_address" {
+  type = string
+}
+
+variable "slack_channel_id" {
+  type = string
+}
+
+variable "slack_workspace_id" {
+  type = string
+}
+
+variable "aws_account_id" {
+  type = string
+}
+
+variable "threshold" {
+  type = string
+}
+
+variable "limit_amount" {
+  type = string
+}
